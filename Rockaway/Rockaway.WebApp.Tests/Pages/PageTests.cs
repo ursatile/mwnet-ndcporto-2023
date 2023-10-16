7da4d457f5d0ccbd6839504ed64f728cebe91d85
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-16 10:56:02 +0100

    Fixed all the tests!

--- Rockaway/Rockaway.WebApp.Tests/Pages/PageTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/Pages/PageTests.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using AngleSharp;
 using Shouldly;
 
-public class PageTests {
+public class PageTests
+{
 
 	[Fact]
-	public async Task Homepage_Works() {
+	public async Task Homepage_Works()
+	{
 		var factory = new WebApplicationFactory<Program>();
 		var client = factory.CreateClient();
 		var result = await client.GetAsync("/");
@@ -17,7 +19,8 @@ public class PageTests {
 	[InlineData("/")]
 	[InlineData("/privacy")]
 	// [InlineData("/contact")]
-	public async Task Page_Works(string url) {
+	public async Task Page_Works(string url)
+	{
 		var factory = new WebApplicationFactory<Program>();
 		var client = factory.CreateClient();
 		var result = await client.GetAsync(url);
@@ -25,7 +28,8 @@ public class PageTests {
 	}
 
 	[Fact]
-	public async Task Homepage_Title_Has_Correct_Content() {
+	public async Task Homepage_Title_Has_Correct_Content()
+	{
 		var browsingContext = BrowsingContext.New(Configuration.Default);
 		var factory = new WebApplicationFactory<Program>();
 		var client = factory.CreateClient();
@@ -35,5 +39,22 @@ public class PageTests {
 		title.ShouldNotBeNull();
 		title.InnerHtml.ShouldBe("Rockaway");
 	}
+
+	[Theory]
+	[InlineData("/", "Rockaway")]
+	[InlineData("/privacy", "Privacy Policy - Rockaway")]
+	[InlineData("/contact", "Contact Us - Rockaway")]
+	public async Task Page_Has_Correct_Title(string url, string title)
+	{
+		var browsingContext = BrowsingContext.New(Configuration.Default);
+		var factory = new WebApplicationFactory<Program>();
+		var client = factory.CreateClient();
+		var html = await client.GetStringAsync(url);
+		var dom = await browsingContext.OpenAsync(req => req.Content(html));
+		var titleElement = dom.QuerySelector("title");
+		titleElement.ShouldNotBeNull();
+		titleElement.InnerHtml.ShouldBe(title);
+	}
+
 
 }
