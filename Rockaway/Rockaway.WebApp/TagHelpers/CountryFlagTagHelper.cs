71d2010c380b390fc47b4d22587273243391a1b3
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-17 16:05:42 +0100

    Country code working with helper attributes! Yay!

--- Rockaway/Rockaway.WebApp/TagHelpers/CountryFlagTagHelper.cs
+++ Rockaway/Rockaway.WebApp/TagHelpers/CountryFlagTagHelper.cs
@@ -4,11 +4,12 @@ namespace Rockaway.WebApp.TagHelpers;
 
 public class CountryFlagTagHelper : TagHelper {
 
+	public string CountryCode { get; set; } = String.Empty;
 	public override void Process(TagHelperContext context, TagHelperOutput output) {
-		var countryCode = context.AllAttributes["countryCode"].Value;
+		// var countryCode = context.AllAttributes["countryCode"].Value;
 		output.TagName = "img";
 		output.TagMode = TagMode.SelfClosing;
-		output.Attributes.Add("src", $"/img/flags/{countryCode}.png");
+		output.Attributes.Add("src", $"/img/flags/{CountryCode}.png");
 		output.Attributes.Add("class", "country-flag");
 	}
 }
